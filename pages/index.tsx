5c4fb78ff79b2202f3727684de2a9a28d80a7c2b
Barto Molina <bartomolina@gmail.com>
2022-12-14 12:19:04 +0100

    Added navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Blockscan</title>
-        <meta name="description" content="Blockscan Block Explorer" />
+        <title>BlockScan</title>
+        <meta name="description" content="BlockScan Block Explorer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <header>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Latest blocks</h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="px-4 py-8 sm:px-0">
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200">
+              Latest blocks
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
